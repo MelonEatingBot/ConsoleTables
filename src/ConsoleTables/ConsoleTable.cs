62e673e41925ad6fd2c2b51ef7f0352b622d6a58
MelonEatingBot <limjingchun@1utar.my>
2020-08-08 16:48:54 +0800

    Docs(ConsoleTable.cs): Added random comments

--- src/ConsoleTables/ConsoleTable.cs
+++ src/ConsoleTables/ConsoleTable.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
-
+//some random comments
 namespace ConsoleTables
 {
     public class ConsoleTable
